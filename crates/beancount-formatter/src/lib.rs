7637d9be426be5398e415b744678c92df1e9ae31
trim21 <i@trim21.me>
2026-01-16 16:14:38 +0800

    fix for was32

--- crates/beancount-formatter/src/lib.rs
+++ crates/beancount-formatter/src/lib.rs
@@ -5,13 +5,3 @@ mod parse;
 pub use beancount_parser::ParseError;
 pub use format::format;
 
-/// Parse file into typed directives.
-///
-/// This is primarily intended for tests and debugging.
-pub fn parse_directives_with_meta<'a>(
-  root: tree_sitter::Node,
-  source: &'a str,
-  filename: String,
-) -> Result<Vec<beancount_parser::ast::Directive<'a>>, ParseError> {
-  beancount_parser::parse_directives_with_meta(root, source, filename)
-}
